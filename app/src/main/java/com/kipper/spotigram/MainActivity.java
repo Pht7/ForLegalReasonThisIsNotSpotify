6d42b2e18454077dbabf686c7e46dfdd6721316b
Karim Gueye <kgueye2000@gmail.com>
2021-11-04 10:28:24 -0400

    First commit

---
+++ app/src/main/java/com/kipper/spotigram/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kipper.spotigram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
